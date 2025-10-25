cdca506aa0aec0be765feaeddd9dfc6a55ca72eb
jaideep singh <jsofficial7755@gmail.com>
2025-10-25 13:41:19 +0530

    update clerk

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import { connectDB } from './config/database.js'
+import clerkWebhooks from './controllers/webhooks.js'
 
 dotenv.config({
     path : './.env'
@@ -21,6 +22,8 @@ app.use(cors({credentials : true}));
 app.get('/', (req,res)=>{
     res.send("Welcome to Edemy!")
 })
+
+app.post('/clerk', express.json(),clerkWebhooks)
 
 // Connect to DataBase !! 
  connectDB()
